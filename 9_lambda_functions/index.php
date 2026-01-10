7a5ad6d68b832c779b9795c39ee81d62bd411b12
Victor Elgersma <elgersmav@gmail.com>
2026-01-10 12:22:21 +0100

    finished lambda functions

---
+++ 9_lambda_functions/index.php
@@ -0,0 +1,61 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Demo</title>
+        <style>
+        body {
+            display: grid;
+              place-content: center;
+              height: 100vh;
+              font-family: sans-serif;
+              margin: 0;
+        }
+        </style>
+    </head>
+    <body>
+
+        <?php
+        $books = [
+            [
+                "name" => "Do Androids Dream of Electric Sheep",
+                "author" => "Philip K. Dick",
+                "releaseYear" => 1989,
+                "purchaseUrl" => "http://example.com",
+            ],
+            [
+                "name" => "Project Hail Mary",
+                "author" => "Andy Weir",
+                "releaseYear" => 1968,
+                "purchaseUrl" => "hello",
+            ],
+            [
+                "name" => "The Martian",
+                "author" => "Andy Weir",
+                "releaseYear" => 2011,
+                "purchaseUrl" => "http://example.com",
+            ],
+        ];
+
+        $filteredBooks = array_filter($books, function ($book) {
+            return $book["author"] === "Andy Weir";
+        });
+        ?>
+
+        <ul>
+            <?php foreach ($filteredBooks as $item): ?>
+                <!--  https://www.php.net/manual/en/control-structures.alternative-syntax.php -->
+                    <li>
+                        <a href="<?= $item["purchaseUrl"] ?>"> <?= $item[
+    "name"
+] ?>
+                        (<?= $item["releaseYear"] ?>) - By <?= $item[
+    "author"
+] ?>
+                        </a>
+                    </li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
